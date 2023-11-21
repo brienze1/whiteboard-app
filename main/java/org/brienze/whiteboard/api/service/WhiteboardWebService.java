d2ab9dd29ab7fe2700dddc5ca7f4f66de0d6d6f0
Luis Brienze <lfbrienze@gmail.com>
2023-11-21 00:31:49 +0000

    removed unwanted files, added identifier header to requests

--- main/java/org/brienze/whiteboard/api/service/WhiteboardWebService.java
+++ main/java/org/brienze/whiteboard/api/service/WhiteboardWebService.java
@@ -6,12 +6,15 @@ import org.brienze.whiteboard.api.model.Shape;
 import org.brienze.whiteboard.api.model.WhiteboardState;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Random;
 
 @Component
 public class WhiteboardWebService {
@@ -20,25 +23,29 @@ public class WhiteboardWebService {
     private String url;
 
     private final RestTemplate restTemplate;
+    private final HttpHeaders headers;
+
 
     public WhiteboardWebService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+        this.headers = new HttpHeaders();
+        this.headers.add("application-id", String.valueOf(new Random().nextInt(1000) + 1));
     }
 
     @Async
     @Retryable(retryFor = Exception.class, maxAttempts = 2, backoff = @Backoff(delay = 100))
     public void save(String name, Shape shape) {
-        restTemplate.exchange(url + "/" + name + "/shapes", HttpMethod.POST, new HttpEntity<>(shape.toDto(), null), ShapeDto.class);
+        restTemplate.exchange(url + "/" + name + "/shapes", HttpMethod.POST, new HttpEntity<>(shape.toDto(), headers), ShapeDto.class);
         System.out.println("Save Shape for whiteboard: " + name);
     }
 
     public WhiteboardState getState(String name) {
         WhiteboardStateDto state;
         try {
-            state = restTemplate.exchange(url + "/" + name, HttpMethod.GET, new HttpEntity<>(null), WhiteboardStateDto.class).getBody();
+            state = restTemplate.exchange(url + "/" + name, HttpMethod.GET, new HttpEntity<>(headers), WhiteboardStateDto.class).getBody();
         } catch (Exception ex) {
             state = new WhiteboardStateDto(name);
-            state = restTemplate.exchange(url, HttpMethod.POST, new HttpEntity<>(state, null), WhiteboardStateDto.class).getBody();
+            state = restTemplate.exchange(url, HttpMethod.POST, new HttpEntity<>(state, headers), WhiteboardStateDto.class).getBody();
         }
 
         System.out.println("Get whiteboard state: " + name);
@@ -47,7 +54,7 @@ public class WhiteboardWebService {
     }
 
     public WhiteboardState clearState(String name) {
-        return restTemplate.exchange(url + "/" + name + "/clear", HttpMethod.POST, new HttpEntity<>(null), WhiteboardStateDto.class)
+        return restTemplate.exchange(url + "/" + name + "/clear", HttpMethod.POST, new HttpEntity<>(headers), WhiteboardStateDto.class)
                            .getBody()
                            .toWhiteboardState();
     }
